7be6cc93d444bb49b2eff2b217aa7ef92d400386
Mark Veltzer <mark.veltzer@gmail.com>
2010-07-18 20:08:33 +0300

    gave credit to shlomi

--- mytheme/footer.php
+++ mytheme/footer.php
@@ -6,7 +6,7 @@
 </div><!-- End id=wrapper -->
 <div class="clear">&nbsp;</div>
 <div id="footnote" align="center">
-	Powered by <a href="http://wordpress.org/">Wordpress</a> and <a href="http://www.mysql.com/">MySQL</a>. Theme by <a href="http://veltzer.net">Mark Veltzer</a>
+	Powered by <a href="http://wordpress.org/">Wordpress</a> and <a href="http://www.mysql.com/">MySQL</a>. Theme by <a href="http://veltzer.net">Mark Veltzer</a> derived from <a href="http://openark.org">Shlomi Noachs openark theme</a>
 </div>
 </div><!-- End align=center -->
 </body>
