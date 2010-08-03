f6a6d7b271a74f6a775d1340cd89a8b562b67071
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-03 04:37:43 +0300

    updates...

---
+++ scripts/wp_unlock_full.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this script should be used when you are updating wordpress itself...
+
+BLOGDIR=/var/www/blog/
+HTACCESS=/var/www/.htaccess
+
+sudo chown -R www-data.www-data ${BLOGDIR}
+sudo chmod 666 ${HTACCESS}
