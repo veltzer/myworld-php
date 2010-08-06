24ad95cd5030afd45ba5ff8ee578e388302fc3e9
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-06 12:09:17 +0300

    more utf fixes...

--- sa/test_lilypond.php
+++ sa/test_lilypond.php
@@ -1,6 +1,8 @@
+<?php
+header("Content-type: text/html; charset=UTF-8");
+?>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>Lilypond music</title>
 	</head>
 <body>
--- sa/test_works.php
+++ sa/test_works.php
@@ -1,6 +1,8 @@
+<?php
+header("Content-type: text/html; charset=UTF-8");
+?>
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<title>Works</title>
 	</head>
 <body>
