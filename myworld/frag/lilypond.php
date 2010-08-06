b2910028aae000f30f2afadefc2839145513f154
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-06 21:19:09 +0300

    jQuery now works...

--- myworld/frag/lilypond.php
+++ myworld/frag/lilypond.php
@@ -30,47 +30,51 @@ function create_lilypond() {
 
 	while($row=mysql_fetch_assoc($result)) {
 		$id=$row["id"];
-		$title=val_or_na($row["title"]);
-		$subtitle=val_or_na($row["subtitle"]);
-		$composer=val_or_na($row["composer"]);
-		$copyright=val_or_na($row["copyright"]);
-		$style=val_or_na($row["style"]);
-		$piece=val_or_na($row["piece"]);
-		$poet=val_or_na($row["poet"]);
-		$a_ly=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=ly&type=text/plain&name_field=filebasename");
-		$a_pdf=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=pdf&type=application/pdf&name_field=filebasename");
-		$a_ps=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=ps&type=application/postscript&name_field=filebasename");
-		$a_midi=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=midi&type=audio/midi&name_field=filebasename");
-		$a_ly="<a href='{$a_ly}'>ly</a>";
-		$a_pdf="<a href='{$a_pdf}'>pdf</a>";
-		$a_ps="<a href='{$a_ps}'>ps</a>";
-		$a_midi="<a href='{$a_midi}'>midi</a>";
+		$s_title=val_or_na($row["title"]);
+		$s_subtitle=val_or_na($row["subtitle"]);
+		$s_composer=val_or_na($row["composer"]);
+		$s_copyright=val_or_na($row["copyright"]);
+		$s_style=val_or_na($row["style"]);
+		$s_piece=val_or_na($row["piece"]);
+		$s_poet=val_or_na($row["poet"]);
+		$s_a_ly=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=ly&type=text/plain&name_field=filebasename");
+		$s_a_pdf=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=pdf&type=application/pdf&name_field=filebasename");
+		$s_a_ps=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=ps&type=application/postscript&name_field=filebasename");
+		$s_a_midi=link_to_direct("GetBlob.php?table=TbMsLilypond&id=$id&field=midi&type=audio/midi&name_field=filebasename");
+		$s_a_ly="<a href='{$s_a_ly}'>ly</a>";
+		$s_a_pdf="<a href='{$s_a_pdf}'>pdf</a>";
+		$s_a_ps="<a href='{$s_a_ps}'>ps</a>";
+		$s_a_midi="<a href='{$s_a_midi}'>midi</a>";
 		
 		if($show_style=="table") {
 			$res.="<tr>";
-			$res.="<td>{$title}</td>";
-			$res.="<td>{$subtitle}</td>";
-			$res.="<td>{$a_ly}</td>";
-			$res.="<td>{$a_pdf}</td>";
-			$res.="<td>{$a_ps}</td>";
-			$res.="<td>{$a_midi}</td>";
-			$res.="<td>{$composer}</td>";
-			$res.="<td>{$copyright}</td>";
-			$res.="<td>{$style}</td>";
-			$res.="<td>{$piece}</td>";
-			$res.="<td>{$poet}</td>";
+			$res.="<td>{$s_title}</td>";
+			$res.="<td>{$s_subtitle}</td>";
+			$res.="<td>{$s_a_ly}</td>";
+			$res.="<td>{$s_a_pdf}</td>";
+			$res.="<td>{$s_a_ps}</td>";
+			$res.="<td>{$s_a_midi}</td>";
+			$res.="<td>{$s_composer}</td>";
+			$res.="<td>{$s_copyright}</td>";
+			$res.="<td>{$s_style}</td>";
+			$res.="<td>{$s_piece}</td>";
+			$res.="<td>{$s_poet}</td>";
 			$res.="</tr>\n";
 		}
 		if($show_style=="div") {
-			$res.="<h3>{$title} / {$composer}</h3>";
-			$res.="<p><ul>";
-			$res.="<li>subtitle: ${subtitle}</li>";
-			$res.="<li>composer: ${composer}</li>";
-			$res.="<li>style: ${style}</li>";
-			$res.="<li>piece: ${piece}</li>";
-			$res.="<li>poet: ${poet}</li>";
-			$res.="<li>links ${a_ly}, ${a_pdf}, ${a_ps}, ${a_midi}</li>";
-			$res.="</ul></p>";
+			$header=$s_title;
+			if($row["composer"]!=NULL) {
+				$header.=" / ".$s_composer;
+			}
+			$res.="<h3>{$header}</h3>";
+			$res.="<div><ul>";
+			$res.="<li>subtitle: ${s_subtitle}</li>";
+			$res.="<li>composer: ${s_composer}</li>";
+			$res.="<li>style: ${s_style}</li>";
+			$res.="<li>piece: ${s_piece}</li>";
+			$res.="<li>poet: ${s_poet}</li>";
+			$res.="<li>links ${s_a_ly}, ${s_a_pdf}, ${s_a_ps}, ${s_a_midi}</li>";
+			$res.="</ul></div>";
 		}
 	}
 	assert(mysql_free_result($result));
--- myworld/javascript/mysupport.js
+++ myworld/javascript/mysupport.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function() {
 	//jQuery(".accordion2 h3").eq(2).addClass("active");
 	//jQuery(".accordion2 p").eq(2).show();
 	jQuery(".accordion2 h3").click(function(){
-		jQuery(this).next("p").slideToggle("fast")
+		//jQuery(this).next("p").slideToggle("fast");
+		//jQuery(this).next("p").children("*").slideToggle();
+		jQuery(this).next("div").slideToggle();
 		//siblings("p:visible").slideUp("slow");
 		jQuery(this).toggleClass("active");
 		jQuery(this).siblings("h3").removeClass("active");
