cbb9ff9b237fc1e26d5eec76e2c4e0d940ff281b
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-24 02:08:16 +0300

    put back google calendar

--- myworld/javascript/myworld.js
+++ myworld/javascript/myworld.js
@@ -8,8 +8,9 @@ jQuery(document).ready(function() {
 	jQuery('#calendar').fullCalendar({
 		editable: false,
 		//events: jQuery.fullCalendar.gcalFeed('http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic'),
-		//events: jQuery.fullCalendar.gcalFeed('http://www.google.com/calendar/feeds/fcu5o54l00cmskelfcq9q3t4kc%40group.calendar.google.com/public/basic'),
-		events: "../wp-content/plugins/myworld/sa/GetEvents.php",
+		events: jQuery.fullCalendar.gcalFeed('http://www.google.com/calendar/feeds/fcu5o54l00cmskelfcq9q3t4kc%40group.calendar.google.com/public/basic'),
+		// TODO: this next url is UGLY - fix it...
+		//events: "../wp-content/plugins/myworld/sa/GetEvents.php",
 		eventClick: function(event) {
 			// opens events in a popup window
 			window.open(event.url, 'gcalevent', 'width=700,height=600');
