79ca2eea7a3ccf85988b397056dbdb7eca380207
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-23 22:39:55 +0300

    calendar now actually works

--- myworld/javascript/myworld.js
+++ myworld/javascript/myworld.js
@@ -7,7 +7,8 @@ jQuery(document).ready(function() {
 	// this is the calendar support fuction
 	jQuery('#calendar').fullCalendar({
 		editable: false,
-		events: jQuery.fullCalendar.gcalFeed('http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic'),
+		//events: jQuery.fullCalendar.gcalFeed('http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic'),
+		events: jQuery.fullCalendar.gcalFeed('http://www.google.com/calendar/feeds/fcu5o54l00cmskelfcq9q3t4kc%40group.calendar.google.com/public/basic'),
 		eventClick: function(event) {
 			// opens events in a popup window
 			window.open(event.url, 'gcalevent', 'width=700,height=600');
--- myworld/myworld.php
+++ myworld/myworld.php
@@ -110,18 +110,18 @@ if(!class_exists('MyWorld')) {
 		 */
 		function wp_head() {
 			// jquery and jquery ui
-			echo "<script type='text/javascript' src='".plugins_url('javascript/jquery/jquery.js?ver=',__FILE__).$this->version."'></script>'\n";
-			echo "<script type='text/javascript' src='".plugins_url('javascript/jquery/jquery-ui-custom.js?ver=',__FILE__).$this->version."'></script>'\n";
+			echo "<script type='text/javascript' src='".plugins_url('javascript/jquery/jquery.js?ver=',__FILE__).$this->version."'></script>\n";
+			echo "<script type='text/javascript' src='".plugins_url('javascript/jquery/jquery-ui-custom.js?ver=',__FILE__).$this->version."'></script>\n";
 
 			// full calendar stuff
 			echo "<link rel='stylesheet' id='fullcalendar-css' href='".plugins_url('javascript/fullcalendar/fullcalendar.css?ver=',__FILE__).$this->version."' type='text/css' media='screen' />\n";
-			echo "<script type='text/javascript' src='".plugins_url('javascript/fullcalendar/fullcalendar.min.js?ver=',__FILE__).$this->version."'></script>'\n";
-			echo "<script type='text/javascript' src='".plugins_url('javascript/fullcalendar/gcal.js?ver=',__FILE__).$this->version."'></script>'\n";
+			echo "<script type='text/javascript' src='".plugins_url('javascript/fullcalendar/fullcalendar.min.js?ver=',__FILE__).$this->version."'></script>\n";
+			echo "<script type='text/javascript' src='".plugins_url('javascript/fullcalendar/gcal.js?ver=',__FILE__).$this->version."'></script>\n";
 			
 			// This next line is supposed to work but it doesn't...
 			//wp_enqueue_script('myworld', plugins_url('javascript/myworld.js', __FILE__), array('jquery'), '1.0');
 			// I used direct inclusion like below...
-			echo "<script type='text/javascript' src='".plugins_url('javascript/myworld.js?ver=',__FILE__).$this->version."'></script>'\n";
+			echo "<script type='text/javascript' src='".plugins_url('javascript/myworld.js?ver=',__FILE__).$this->version."'></script>\n";
 			// and now for the style sheet...
 			echo "<link rel='stylesheet' id='myworld-css' href='".plugins_url('css/myworld.css?ver=',__FILE__).$this->version."' type='text/css' media='screen' />\n";
 			echo "<link rel='SHORTCUT ICON' type='image/x-icon' href='".plugins_url('resources/favicon.ico',__FILE__)."' />\n";
