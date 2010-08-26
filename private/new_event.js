e8d49f991ff9a37e0037ce434c90db32961e460c
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-27 02:58:07 +0300

    event system is still not done

--- private/new_event.js
+++ private/new_event.js
@@ -1,24 +1,35 @@
 jQuery(document).ready(function() {
-	$("#from_date").datepicker();
-	$("#to_date").datepicker();
-	function log(message) {
-		$("<div/>").text(message).prependTo("#log");
-		$("#log").attr("scrollTop", 0);
+	jQuery.fn.disable=function() {
+		jQuery(this).attr("disabled", true);
 	}
-	$("#course").autocomplete({
+	jQuery.fn.enable=function() {
+		jQuery(this).attr("disabled", false);
+	}
+	jQuery("#from_date").datepicker();
+	jQuery("#to_date").datepicker();
+	function log(message) {
+		// add the message to the log
+		jQuery("<div/>").text(message).appendTo("#log");
+		// scroll to the bottom
+		jQuery("#log").attr("scrollTop", jQuery("#log").attr("scrollHeight"));
+	}
+	jQuery("#course").autocomplete({
 		source: "GetList.php?type=courses",
 		minLength: 0,
 		select: function(event, ui) {
 			log(ui.item ? ("Course selected: " + ui.item.value + " aka " + ui.item.id) : "Nothing selected, input was " + this.value);
+			jQuery("#submit").enable();
 		}
 	});
-	$("#company").autocomplete({
+	jQuery("#company").autocomplete({
 		source: "GetList.php?type=companies",
 		minLength: 0,
 		select: function(event, ui) {
 			log(ui.item ? ("Company selected: " + ui.item.value + " aka " + ui.item.id) : "Nothing selected, input was " + this.value);
+			jQuery("#submit").enable();
 		}
 	});
+	// set styles for focus on the field currently being filled...
 	jQuery('.inputfield').css({backgroundColor:"#FFFFFF"});
 	jQuery('.inputfield').focus(function(){
 		jQuery(this).css({backgroundColor:"#EB8F00"});
@@ -26,19 +37,17 @@ jQuery(document).ready(function() {
 	jQuery('.inputfield').blur(function(){
 		jQuery(this).css({backgroundColor:"#FFFFFF"});
 	});
+	// set focus to the first input field
 	jQuery("input#company").select().focus();
+	// disable sumbit on first entering the form
+	jQuery("#submit").disable();
 	jQuery("#submit").click(function() {
-		// validate and process form
-		// first hide any error messages
-		var dataString='';
-		var p_company=jQuery("input#company").val();
-		dataString+='company='+p_company;
-		var p_course=jQuery("input#course").val();
-		dataString+='&course='+p_course;
-		var p_from_date=jQuery("input#from_date").val();
-		dataString+='&from_date='+p_from_date;
-		var p_to_date=jQuery("input#to_date").val();
-		dataString+='&to_date='+p_to_date;
+		// disable the submit button to prevent double sumbit
+		jQuery("#submit").disable();
+		// multi validations (validations involving several fields)
+		// I don't have any such validations at the moment.
+		// serialize all data (this is a jQuery function)
+		var dataString=$('#myform').serialize();
 		log("submitting: "+dataString);
 		jQuery.ajax({
 			type: "POST",
@@ -47,8 +56,13 @@ jQuery(document).ready(function() {
 			error: function(data) {
 				log(data);
 			},
+			// only on success
 			success: function(data) {
 				log(data);
+			},
+			// function which is called on erorr on success
+			complete: function() {
+				jQuery("#submit").enable();
 			}
 		});
 		return false;
