8547a23c178c3454e1cd23a0dec3b6ef2b4fc3be
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-27 04:26:39 +0300

    added ajax monitor

--- private/NewEvent.js
+++ private/NewEvent.js
@@ -1,42 +1,11 @@
 jQuery(document).ready(function() {
+	// enhance jQuery to have disable/enable functions
 	jQuery.fn.disable=function() {
 		jQuery(this).attr("disabled", true);
 	}
 	jQuery.fn.enable=function() {
 		jQuery(this).attr("disabled", false);
 	}
-	jQuery("#from_date").datepicker();
-	jQuery("#to_date").datepicker();
-	function log(message) {
-		// add the message to the log
-		jQuery("<div/>").text(message).appendTo("#log");
-		// scroll to the bottom
-		jQuery("#log").attr("scrollTop", jQuery("#log").attr("scrollHeight"));
-	}
-	jQuery("#course").disable();
-	jQuery.getJSON('GetList.php?type=course_all', function(data) {
-		jQuery("#course").autocomplete({
-			source: data,
-			minLength: 2,
-			select: function(event, ui) {
-				log(ui.item ? ("Course selected: " + ui.item.value + " aka " + ui.item.id) : "Nothing selected, input was " + this.value);
-				jQuery("#submit").enable();
-			}
-		});
-		jQuery("#course").enable();
-	});
-	jQuery("#company").disable();
-	jQuery.getJSON('GetList.php?type=company_all', function(data) {
-		jQuery("#company").autocomplete({
-			source: data,
-			minLength: 2,
-			select: function(event, ui) {
-				log(ui.item ? ("Company selected: " + ui.item.value + " aka " + ui.item.id) : "Nothing selected, input was " + this.value);
-				jQuery("#submit").enable();
-			}
-		});
-		jQuery("#company").enable();
-	});
 	// set styles for focus on the field currently being filled...
 	jQuery('.inputfield').css({backgroundColor:"#FFFFFF"});
 	jQuery('.inputfield').focus(function(){
@@ -45,8 +14,55 @@ jQuery(document).ready(function() {
 	jQuery('.inputfield').blur(function(){
 		jQuery(this).css({backgroundColor:"#FFFFFF"});
 	});
+	// general logging function
+	function log(message) {
+		// add the message to the log
+		jQuery("<div/>").text(message).appendTo("#log");
+		// scroll to the bottom
+		jQuery("#log").attr("scrollTop", jQuery("#log").attr("scrollHeight"));
+	}
+	jQuery(document).ajaxSend(function(event,request,settings) {
+		log('ajaxStart '+settings.url);
+	});
+	jQuery(document).ajaxComplete(function(event,request,settings) {
+		log('ajaxComplete '+settings.url);
+	});
+
+	// construct two date pickers
+	jQuery("#from_date").datepicker();
+	jQuery("#to_date").datepicker();
+	jQuery("#company").disable();
+	jQuery("#company").val('getting data...');
+	jQuery.getJSON('GetList.php?type=company_all', function(data) {
+		jQuery("#company").autocomplete({
+			source: data,
+			minLength: 2,
+			select: function(event, ui) {
+				log(ui.item ? ("Company selected: " + ui.item.value + " aka " + ui.item.id) : "Nothing selected, input was " + this.value);
+			}
+		});
+		// now company can be selected
+		jQuery("#company").enable();
+		// set error state as non selected
+		jQuery("#company").val(data[0].label);
+	});
+	jQuery("#course").disable();
+	jQuery("#course").val('getting data...');
+	jQuery.getJSON('GetList.php?type=course_all', function(data) {
+		jQuery("#course").autocomplete({
+			source: data,
+			minLength: 2,
+			select: function(event, ui) {
+				log(ui.item ? ("Course selected: " + ui.item.value + " aka " + ui.item.id) : "Nothing selected, input was " + this.value);
+			}
+		});
+		// now course can be selected
+		jQuery("#course").enable();
+		// set error state as non selected
+		jQuery("#course").val(data[0].label);
+	});
 	// set focus to the first input field
-	jQuery("input#company").select().focus();
+	//jQuery("input#company").select().focus();
 	// disable sumbit on first entering the form
 	jQuery("#submit").disable();
 	jQuery("#submit").click(function() {
@@ -70,6 +86,7 @@ jQuery(document).ready(function() {
 			},
 			// function which is called on erorr on success
 			complete: function() {
+				// TODO: need to check for validations
 				jQuery("#submit").enable();
 			}
 		});
