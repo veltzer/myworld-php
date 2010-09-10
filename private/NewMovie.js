9b5a302467a534cf2378848bad1fae8d664230f8
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-10 17:21:07 +0300

    my first jquery ui widget

--- private/NewMovie.js
+++ private/NewMovie.js
@@ -1,44 +1,53 @@
 jQuery(document).ready(function() {
 	var init={
 		'name':'Name',
-		'initMsg': 'put the name of the movie here',
-		'regex' : /.+/,
+		'initMsg':'put the name of the movie here',
+		'regex':/.+/,
 	};
 	jQuery('#name').jvalidfield(init);
 
 	var init={
 		'name':'Imdbid',
-		'initMsg': 'put the imdbid here',
-		'regex' : /^\d{7}$/,
+		'initMsg':'put the imdbid here',
+		'regex':/^\d{7}$/,
 	};
 	jQuery('#imdbid').jvalidfield(init);
 	
 	var init={
 		'name':'Date',
-		'initMsg': 'put the date seen here',
-		'regex' : /.+/,
+		'initMsg':'put the date seen here',
+		'regex':/.+/,
+		'validate':function(widget,value) {
+			var t=Date.parse(value);
+			return !isNaN(t);
+		},
+		'validate_error':function(widget,value) {
+			return 'could not parse date object';
+		},
 	};
 	jQuery('#date').jvalidfield(init);
 
 	var init={
-		'name': 'Rating',
-		'initMsg': 'put the rating (1-10) here',
+		'name':'Rating',
+		'initMsg':'put the rating (1-10) here',
 		'url':'GetList.php?table=TbRating',
 	};
 	jQuery('#rating').jurlfield(init);
 
 	var init={
-		'name': 'Location',
-		'initMsg': 'put the location where you saw the movie',
+		'name':'Location',
+		'initMsg':'put the location where you saw the movie',
 		'url':'GetList.php?table=TbLcNamed',
 	};
 	jQuery('#location').jurlfield(init);
 
 	var init={
 		'name':'Review',
-		'initMsg': 'Put your review here',
-		'type': 'textarea',
-		'regex' : /.+/,
+		'initMsg':'Put your review here',
+		'type':'textarea',
+		'regex':/.+/,
 	};
 	jQuery('#review').jvalidfield(init);
+
+	jQuery('#combobox').combobox();
 });
--- private/jquery.jvalidfield.js
+++ private/jquery.jvalidfield.js
@@ -3,95 +3,94 @@
  * You can pass any regexp to validate against.
  */
 jQuery(document).ready(function() {
-	jQuery.fn.doFocusin=function() {
-		var widget=jQuery(this);
-		var w_input=widget.data('w_input');
-		var options=widget.data('options');
-		widget.addClass(options.activeClass);
-		if(widget.data('initState')==true) {
-			// reset the value
-			w_input.val('');
-			widget.data('initState',false);
-			widget.validate();
-		}
-	}
-	jQuery.fn.doFocusout=function() {
-		var widget=jQuery(this);
-		var w_input=widget.data('w_input');
-		var options=widget.data('options');
-		widget.removeClass(options.activeClass);
-		widget.validate();
-	}
-	jQuery.fn.validate=function() {
-		var widget=jQuery(this);
-		var options=widget.data('options');
-		var w_input=widget.data('w_input');
-		if(widget.data('initState')==true) {
-			widget.setError('no data entered');
-			return;
-		}
-		if(options.regex.test(w_input.val())) {
-			widget.setOk();
-		} else {
-			widget.setError('regex error '+options.regex);
-		}
-	}
-	jQuery.fn.extend({
-		jvalidfield: function(options) {
-			var defaults = {
-				regex: /.*/,
-				addLabel: true,
-				type: 'input',
-				inputtype: 'text',
-				name: 'No name',
-				initMsg: 'put some text here',
-				activeClass: 'focus',
-				mustInput: true,
-				rows: 10,
-				initError: true,
+	jQuery.widget("ui.jvalidfield",{
+		options:{
+			regex: null,
+			addLabel: true,
+			type: 'input',
+			inputtype: 'text',
+			name: 'No name',
+			initMsg: 'put some text here',
+			mustInput: true,
+			rows: 10,
+			initError: true,
+			initState: true,
+			validate: function(widget,value) {
+				return widget.options.regex.test(value);
+			},
+			validate_error:function(widget,value) {
+				return 'regex error '+widget.options.regex;
+			},
+		},
+		setError:function(errormsg) {
+			this.w_error.html(errormsg);
+			this.error=true;
+		},
+		setOk:function() {
+			this.w_error.html('');
+			this.error=false;
+		},
+		doFocusin:function() {
+			this.element.addClass('focus');
+			if(this.options.initState==true) {
+				// reset the value
+				this.w_input.val('');
+				this.options.initState=false;
+				this.validate();
+			}
+		},
+		doFocusout:function() {
+			this.element.removeClass('focus');
+			this.validate();
+		},
+		validate:function() {
+			if(this.options.initState==true) {
+				this.setError('no data entered');
+				return;
+			}
+			if(this.options.validate(this,this.w_input.val())) {
+				this.setOk();
+			} else {
+				var error=this.options.validate_error(this,this.w_input.val());
+				this.setError(error);
+			}
+		},
+		_create:function() {
+			this.element.addClass('ui-widget');
+			var widget=this;
+			if(this.options.addLabel==true) {
+				this.w_label=jQuery('<label>');
+				this.w_label.html(this.options.name);
+				this.w_label.addClass('fieldlabel');
+				this.w_label.appendTo(this.element);
+			}
+			var attrs={
+				val: this.options.initMsg,
+				focusin: function() {
+					widget.doFocusin();
+				},
+				focusout: function() {
+					widget.doFocusout();
+				},
+				keyup: function() {
+					widget.validate();
+				},
 			};
-			var o=jQuery.extend(defaults, options);
-			return this.each(function() {
-				var widget=jQuery(this);
-				widget.addClass('ui-widget');
-				widget.data('options',o);
+			if(this.options.type=='input') {
+				attrs.type=this.options.inputtype;
+			}
+			if(this.options.type=='textarea') {
+				attrs.rows=this.options.rows;
+			}
+			this.w_input=jQuery('<'+this.options.type+'>',attrs);
+			this.w_input.addClass('anyinput');
+			this.w_input.appendTo(this.element);
 
-				if(o.addLabel==true) {
-					var w_label=jQuery('<label>',{}).html(o.name).appendTo(this);
-					w_label.addClass('fieldlabel');
-					widget.data('w_label',w_label);
-				}
-				var attrs={
-					val: o.initMsg,
-					focusin: function() {
-						widget.doFocusin();
-					},
-					focusout: function() {
-						widget.doFocusout();
-					},
-					keyup: function() {
-						widget.validate();
-					},
-				}
-				if(o.type=='input') {
-					attrs.type=o.inputtype;
-				}
-				if(o.type=='textarea') {
-					attrs.rows=o.rows;
-				}
-				var w_input=jQuery('<'+o.type+'>',attrs);
-				w_input.addClass('anyinput');
-				w_input.appendTo(this);
-				widget.data('w_input',w_input);
+			this.w_error=jQuery('<label>');
+			this.w_error.addClass('validation_error');
+			this.w_error.appendTo(this.element);
 
-				var w_error=jQuery('<label>');
-				w_error.addClass('validation_error');
-				w_error.appendTo(this);
-				widget.data('w_error',w_error);
-
-				widget.data('initState',true);
-				widget.validate();
-			});
-		}
+			this.validate();
+		},
 	});
 });
