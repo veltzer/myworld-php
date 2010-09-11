a3c83a3b677852bc11fc78c6cde9ad77d0f8ceee
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-11 14:40:04 +0300

    clean up

--- private/GetData.php
+++ private/GetData.php
@@ -3,6 +3,10 @@ require('utils.php');
 utils_init();
 $type=my_get_get('type');
 $handled=0;
+if($type=='jsondata') {
+	$response='[ { "id": "Nycticorax nycticorax", "label": "Black-crowned Night Heron", "value": "Black-crowned Night Heron" }, { "id": "Ardea purpurea", "label": "Purple Heron", "value": "Purple Heron" }, { "id": "Ardea cinerea", "label": "Grey Heron", "value": "Grey Heron" }, { "id": "Anser albifrons", "label": "Greater White-fronted Goose", "value": "Greater White-fronted Goose" }, { "id": "Anser erythropus", "label": "Lesser White-fronted Goose", "value": "Lesser White-fronted Goose" }, { "id": "Ardeola ralloides", "label": "Squacco Heron", "value": "Squacco Heron" }, { "id": "Serinus pusillus", "label": "Red-Fronted Serin", "value": "Red-Fronted Serin" }, { "id": "Butorides virescens", "label": "Green Heron", "value": "Green Heron" } ]';
+	$handled=1;
+}
 if($type=='video_devices') {
 	$query=sprintf('select id,name as label,name as value from TbDevice where isVideo=1');
 	$result=my_mysql_query($query);
