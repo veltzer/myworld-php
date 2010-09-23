e09e4dd0fe2a2ec93d29ee2feb736f0803e6f724
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-23 16:10:44 +0200

    fixed up NewMovie.php to match new Rating system

--- private/NewMovie.php
+++ private/NewMovie.php
@@ -9,33 +9,35 @@ if($debug) {
 # parameters for this script...
 $p_name=my_get_post('name');
 $p_imdbid=my_get_post('imdbid');
-$p_date=my_get_post('date');
-$p_date=javascriptdate_to_mysqldate($p_date);
+$p_date=javascriptdate_to_mysqldate(my_get_post('date'));
 $p_locationid=my_get_post('locationid');
 $p_deviceid=my_get_post('deviceid');
 $p_rating=my_get_post('rating');
 $p_review=my_get_post('review');
 
+// this is a line you can use for debugging...
 //error('query not yet implemented');
 
 $p_typeid=my_mysql_query_one('select id from TbWkWorkType where name=\'video movie\'');
-// TODO: maybe a query will be better ?!?
-$p_viewerid=1;
+$p_viewerid=my_mysql_query_one('select id from TbIdPerson where firstname=\'Mark\' and surname=\'Veltzer\'');
 
-$query=sprintf('insert into TbWkWork (name,imdbid,endViewDate,locationId,deviceId,rating,review,typeId,viewerId) values(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')',
+$query=sprintf('insert into TbWkWork (name,imdbid,endViewDate,locationId,deviceId,typeId,viewerId) values(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')',
 	mysql_real_escape_string($p_name),
 	mysql_real_escape_string($p_imdbid),
 	mysql_real_escape_string($p_date),
 	mysql_real_escape_string($p_locationid),
 	mysql_real_escape_string($p_deviceid),
-	mysql_real_escape_string($p_rating),
-	mysql_real_escape_string($p_review),
 	mysql_real_escape_string($p_typeid),
 	mysql_real_escape_string($p_viewerid)
 );
-if($debug) {
-	echo 'query is '.$query;
-}
+my_mysql_query($query);
+$p_workId=mysql_insert_id();
+$query=sprintf('insert into TbWkWorkReview (rating,review,reviewDate,workId) values(\'%s\',\'%s\',\'%s\',\'%s\')',
+	mysql_real_escape_string($p_rating),
+	mysql_real_escape_string($p_review),
+	mysql_real_escape_string($p_date),
+	mysql_real_escape_string($p_workId)
+);
 my_mysql_query($query);
 echo 'new movie successfully inserted';
 ?>
