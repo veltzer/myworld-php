0d6de63b3baf424fe2e204dc27e4b1c0245d4607
Mark Veltzer <mark.veltzer@gmail.com>
2010-11-16 22:03:45 +0200

    new work now works

--- private/NewWork.php
+++ private/NewWork.php
@@ -8,9 +8,9 @@ if($debug) {
 
 # parameters for this script...
 $p_name=my_get_post_or_null('name');
-$p_typeid=my_get_post_or_null('typeid');
-$p_externalid=my_get_post_or_null('externalid');
-$p_externalcode=my_get_post_or_null('externalcode');
+$p_typeid=my_get_post_or_null('typeId');
+$p_externalid=my_get_post_or_null('externalId');
+$p_externalcode=my_get_post_or_null('externalCode');
 
 $query=sprintf('insert into TbWkWork (name,typeId,externalId,externalCode) values(%s,%s,%s,%s)',
 	my_mysql_real_escape_string($p_name),
