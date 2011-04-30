d52161a5a9efa18da5b95032d51341780b5ce1b9
Mark Veltzer <mark.veltzer@gmail.com>
2011-04-30 07:19:01 +0300

    more strings

--- myworld/sa/setup.php
+++ myworld/sa/setup.php
@@ -1,15 +1,15 @@
 <?php
 
 function my_include($page) {
-	include_once("../".$page);
+	include_once('../'.$page);
 }
 
 function link_to_direct($direct_page) {
-	return "../direct/".$direct_page;
+	return '../direct/'.$direct_page;
 }
 
 function link_to_resource($resource) {
-	return "../resources/".$resource;
+	return '../resources/'.$resource;
 }
 
 ?>
