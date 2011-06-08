157d3caeb97c3236dd5df41aa6b003127db5ca37
Mark Veltzer <mark.veltzer@gmail.com>
2011-06-08 03:17:19 +0300

    fixes for new table format

--- private/NewWorkView.php
+++ private/NewWorkView.php
@@ -19,16 +19,22 @@ $p_review=my_get_post('review');
 my_mysql_start_transaction();
 
 // insert a new view
-$query=sprintf('insert into TbWkWorkView (endViewDate,locationId,deviceId,langId,viewerId,workId) values(%s,%s,%s,%s,%s,%s)',
+$query=sprintf('insert into TbWkWorkView (endViewDate,locationId,deviceId,langId,workId) values(%s,%s,%s,%s,%s)',
 	my_mysql_real_escape_string($p_date),
 	my_mysql_real_escape_string($p_locationId),
 	my_mysql_real_escape_string($p_deviceId),
 	my_mysql_real_escape_string($p_langId),
-	my_mysql_real_escape_string($p_personId),
 	my_mysql_real_escape_string($p_workId)
 );
 my_mysql_query($query);
 $p_insertworkviewid=mysql_insert_id();
+// insert the viewer
+$query=sprintf('insert into TbWkWorkViewPerson (viewerId,viewId) values(%s,%s)',
+	my_mysql_real_escape_string($p_personId),
+	my_mysql_real_escape_string($p_insertworkviewid)
+);
+my_mysql_query($query);
+$p_workviewpersonid=mysql_insert_id();
 // insert a new review
 $query=sprintf('insert into TbWkWorkReview (reviewerId,ratingId,review,reviewDate,workId) values(%s,%s,%s,%s,%s)',
 	my_mysql_real_escape_string($p_personId),
@@ -41,5 +47,5 @@ my_mysql_query($query);
 $p_insertworkreviewid=mysql_insert_id();
 my_mysql_commit();
 
-echo "view [{$p_insertworkviewid}] and review [{$p_insertworkreviewid}] successfully inserted";
+echo "inserted view=[{$p_insertworkviewid}],viewer=[{$p_workviewpersonid}],review=[{$p_insertworkreviewid}] ok";
 ?>
