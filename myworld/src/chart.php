beb073d814d494f6ab3d06a532286f1c5a0145fb
Mark Veltzer <mark.veltzer@gmail.com>
2011-11-19 14:08:20 +0200

    more changes

--- myworld/src/chart.php
+++ myworld/src/chart.php
@@ -5,21 +5,29 @@ function create_chart($params) {
 		$limit=$params['limit'];
 	}
 	$sample=<<<EOD
-<div id="chart_id" style="width:260px;height:200px;"></div>
-<script>
-	dojo.require('dojox.charting.Chart2D');
-	dojo.require('dojox.charting.widget.Chart2D');
-	dojo.require('dojox.charting.themes.PlotKit.blue');
-	dojo.require("dojox.charting.StoreSeries");
-	dojo.require("dojo.data.ItemFileReadStore");
-	dojo.ready(function() {
-		var store=new dojo.data.ItemFileReadStore({url:"/private/GetData.php?type=video_viewing"});
-		var storeSeries=new dojox.charting.StoreSeries(store,{ query: { } }, "value"); 
-		var chart = new dojox.charting.Chart2D('chart_id');
-		chart.addSeries('Visits For February', storeSeries);
-		chart.render();
-	});
-</script>
+		<div id="chart_id"></div>
+		<script>
+			dojo.require('dojox.charting.Chart2D');
+			dojo.require('dojox.charting.widget.Chart2D');
+			dojo.require("dojo.data.ItemFileReadStore");
+			dojo.require("dojox.charting.DataChart");
+			dojo.ready(function() {
+				var store=new dojo.data.ItemFileReadStore({url:"/public/GetData.php?type=video_viewing"});
+				var chart=new dojox.charting.DataChart("chart_id", {
+					// this is needed to make nice columns
+					type: dojox.charting.plot2d.Columns,
+					// this is needed. default is true and it sucks big time...
+					scroll:false,
+					// this is needed for the labels
+					xaxis:{labelFunc:"seriesLabels"},
+					// this is needed since y axis is to 10 by default
+					yaxis:{max:60},
+				});
+				// the last two arguments are needed
+				chart.setStore(store, {}, "value");
+				chart.render();
+			});
+		</script>
 EOD;
 	$res='';
 	$res.=$sample;
---
+++ public/utils.php
@@ -0,0 +1,1 @@
+../myworld/src/utils.php
