2c46710c5e1e35345ff2167f2d87ee35325b14f0
Mark Veltzer <mark.veltzer@gmail.com>
2011-11-21 07:37:56 +0200

    can now produce graph data

--- private/GetData.php
+++ private/GetData.php
@@ -8,26 +8,33 @@ if($type=='jsondata') {
 	$handled=1;
 }
 if($type=='video_devices') {
-	$query=sprintf('SELECT id,name AS label,name AS value FROM TbDevice where isVideo=1 ORDER BY name');
+	$query=sprintf('SELECT id,name AS label,name AS value FROM TbDevice WHERE isVideo=1 ORDER BY name');
 	$result=my_mysql_query($query);
 	$response=my_json_encode($result);
 	$handled=1;
 }
 if($type=='video_places') {
-	$query=sprintf('SELECT id,name AS label,name AS value FROM TbLocation where isVideo=1 ORDER BY name');
+	$query=sprintf('SELECT id,name AS label,name AS value FROM TbLocation WHERE isVideo=1 ORDER BY name');
 	$result=my_mysql_query($query);
 	$response=my_json_encode($result);
 	$handled=1;
 }
 if($type=='video_viewing_month') {
-	$query=sprintf('SELECT DATE_FORMAT(TbWkWorkView.endViewDate,"%%m-%%y") AS id,count(*) AS value FROM TbWkWorkViewPerson, TbWkWorkView, TbWkWork, TbWkWorkType where TbWkWorkView.workId=TbWkWork.id AND TbWkWorkViewPerson.viewerId=1 AND TbWkWorkViewPerson.viewId=TbWkWorkView.id AND TbWkWorkType.name="video movie" AND TbWkWorkType.id=TbWkWork.typeId GROUP BY DATE_FORMAT(TbWkWorkView.endViewDate,"%%m-%%y") ORDER BY TbWkWorkView.endViewDate');
+	$query=sprintf('SELECT DATE_FORMAT(TbWkWorkView.endViewDate,"%%m-%%y") AS id,count(*) AS value FROM TbWkWorkViewPerson, TbWkWorkView, TbWkWork, TbWkWorkType WHERE TbWkWorkView.workId=TbWkWork.id AND TbWkWorkViewPerson.viewerId=1 AND TbWkWorkViewPerson.viewId=TbWkWorkView.id AND TbWkWorkType.name="video movie" AND TbWkWorkType.id=TbWkWork.typeId GROUP BY DATE_FORMAT(TbWkWorkView.endViewDate,"%%m-%%y") ORDER BY TbWkWorkView.endViewDate');
 	$result=my_mysql_query($query);
 	# the wrapping is for dojo charting to work...
 	$response='{ label: "id", identifier: "id", items: '.my_json_encode($result).'}';
 	$handled=1;
 }
 if($type=='video_viewing_year') {
-	$query=sprintf('SELECT DATE_FORMAT(TbWkWorkView.endViewDate,"%%y") AS id,count(*) AS value FROM TbWkWorkViewPerson, TbWkWorkView, TbWkWork, TbWkWorkType where TbWkWorkView.workId=TbWkWork.id AND TbWkWorkViewPerson.viewerId=1 AND TbWkWorkViewPerson.viewId=TbWkWorkView.id AND TbWkWorkType.name="video movie" AND TbWkWorkType.id=TbWkWork.typeId GROUP BY DATE_FORMAT(TbWkWorkView.endViewDate,"%%y") ORDER BY TbWkWorkView.endViewDate');
+	$query=sprintf('SELECT DATE_FORMAT(TbWkWorkView.endViewDate,"%%y") AS id,count(*) AS value FROM TbWkWorkViewPerson, TbWkWorkView, TbWkWork, TbWkWorkType WHERE TbWkWorkView.workId=TbWkWork.id AND TbWkWorkViewPerson.viewerId=1 AND TbWkWorkViewPerson.viewId=TbWkWorkView.id AND TbWkWorkType.name="video movie" AND TbWkWorkType.id=TbWkWork.typeId GROUP BY DATE_FORMAT(TbWkWorkView.endViewDate,"%%y") ORDER BY TbWkWorkView.endViewDate');
+	$result=my_mysql_query($query);
+	# the wrapping is for dojo charting to work...
+	$response='{ label: "id", identifier: "id", items: '.my_json_encode($result).'}';
+	$handled=1;
+}
+if($type=='openbook_progress') {
+	$query=sprintf('SELECT DATE_FORMAT(TbGraphData.dt,"%%d%%m%%y") AS id,TbGraphData.value AS value FROM TbGraph, TbGraphData WHERE TbGraph.name=\'openbook_progress\' AND TbGraph.id=TbGraphData.graphId ORDER BY TbGraphData.dt');
 	$result=my_mysql_query($query);
 	# the wrapping is for dojo charting to work...
 	$response='{ label: "id", identifier: "id", items: '.my_json_encode($result).'}';
