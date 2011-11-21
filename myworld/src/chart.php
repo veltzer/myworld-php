9dc2659d99f562d7baf344b7aa67b2c3e3ef5635
Mark Veltzer <mark.veltzer@gmail.com>
2011-11-21 22:55:21 +0200

    better looking charts

--- myworld/src/chart.php
+++ myworld/src/chart.php
@@ -6,18 +6,39 @@ function create_chart($params) {
 	if(!array_key_exists('max',$params)) {
 		return '<strong>Error, must pass max</strong>';
 	}
+	if(!array_key_exists('doWidth',$params)) {
+		return '<strong>Error, must pass doWidth</strong>';
+	}
+	$p_doWidth=$params['doWidth'];
 	$p_type=$params['type'];
 	$p_max=$params['max'];
 	$p_ticks=$p_max/10;
+	if($p_doWidth) {
+		if(!array_key_exists('width',$params)) {
+			return '<strong>Error, must pass width</strong>';
+		}
+		$p_width=$params['width'];
+		$style='style="width: '.$p_width.'"';
+	} else {
+		$style='';
+	}
+	// running id for the divs
 	static $p_id=1;
 	$sample=<<<EOD
-		<div id="chart_$p_id"></div>
+		<div id="scroll_$p_id">
+			<div id="chart_$p_id" $style></div>
+		</div>
 		<script>
 			dojo.require('dojox.charting.Chart2D');
 			dojo.require('dojox.charting.widget.Chart2D');
 			dojo.require("dojo.data.ItemFileReadStore");
 			dojo.require("dojox.charting.DataChart");
+			dojo.require("dojox.layout.ScrollPane");
 			dojo.ready(function() {
+				var scroll=new dojox.layout.ScrollPane({
+					orientation: "horizontal",
+				},"scroll_$p_id");
+				scroll.startup();
 				var store=new dojo.data.ItemFileReadStore({url:"/public/GetData.php?type=$p_type"});
 				var chart=new dojox.charting.DataChart("chart_$p_id", {
 					// this is needed to make nice columns
