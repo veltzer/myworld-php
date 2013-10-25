5182f732c31e6d65d03909d6b4378d1daf6e2b34
Mark Veltzer <mark.veltzer@gmail.com>
2013-10-25 17:49:34 +0300

    phase 1 ok

--- public/myworld_extjs.js
+++ public/myworld_extjs.js
@@ -269,15 +269,113 @@ function create_movies_here() {
 }
 
 function create_chart(loc, type) {
-	Ext.require('Ext.chart.Chart', function () {
-		/*
-		var chart=Ext.create('Ext.chart.Chart', {
+	Ext.require([
+		'Ext.data.JsonStore',
+		'Ext.chart.Chart',
+		'Ext.Window',
+		'Ext.layout.container.Fit',
+		'Ext.fx.target.Sprite',
+		'Ext.window.MessageBox'
+	]
+	, function() {
+		window.generateData = function(n, floor) {
+			var data = [];
+			var p = (Math.random() * 11) + 1;
+			floor = (!floor && floor !== 0)? 20 : floor;
+			for (var i = 0; i < (n || 12); i++) {
+				data.push({
+					name: Ext.Date.monthNames[i % 12],
+					data1: Math.floor(Math.max((Math.random() * 100), floor)),
+					data2: Math.floor(Math.max((Math.random() * 100), floor)),
+					data3: Math.floor(Math.max((Math.random() * 100), floor)),
+					data4: Math.floor(Math.max((Math.random() * 100), floor)),
+					data5: Math.floor(Math.max((Math.random() * 100), floor)),
+					data6: Math.floor(Math.max((Math.random() * 100), floor)),
+					data7: Math.floor(Math.max((Math.random() * 100), floor)),
+					data8: Math.floor(Math.max((Math.random() * 100), floor)),
+					data9: Math.floor(Math.max((Math.random() * 100), floor))
+				});
+			}
+			return data;
+		};
+		window.mystore = Ext.create('Ext.data.JsonStore', {
+			fields: ['name', 'data1', 'data2', 'data3', 'data4', 'data5', 'data6', 'data7', 'data9', 'data9'],
+			data: generateData()
+		});
+		var chart = Ext.create('Ext.chart.Chart', {
+			style: 'background:#fff',
 			animate: true,
 			shadow: true,
-			//store: mystore,
-			renderTo: loc,
+			store: mystore,
+			axes: [{
+				type: 'Numeric',
+				position: 'left',
+				fields: ['data1'],
+				label: {
+					renderer: Ext.util.Format.numberRenderer('0,0')
+				},
+				title: 'Number of Hits',
+				grid: true,
+				minimum: 0
+			}, {
+				type: 'Category',
+				position: 'bottom',
+				fields: ['name'],
+				title: 'Month of the Year'
+			}],
+			series: [{
+				type: 'column',
+				axis: 'left',
+				highlight: true,
+				tips: {
+					trackMouse: true,
+					width: 140,
+					height: 28,
+					renderer: function(storeItem, item) {
+						this.setTitle(storeItem.get('name') + ': ' + storeItem.get('data1') + ' $');
+					}
+				},
+				label: {
+					display: 'insideEnd',
+					'text-anchor': 'middle',
+					field: 'data1',
+					renderer: Ext.util.Format.numberRenderer('0'),
+					orientation: 'vertical',
+					color: '#333'
+				},
+				xField: 'name',
+				yField: 'data1'
+			}]
 		});
-		*/
+		var win = Ext.create('Ext.window.Window', {
+			width: 800,
+			height: 600,
+			minHeight: 400,
+			minWidth: 550,
+			hidden: false,
+			maximizable: true,
+			title: 'Column Chart',
+			autoShow: true,
+			layout: 'fit',
+			tbar: [{
+				text: 'Save Chart',
+				handler: function() {
+				Ext.MessageBox.confirm('Confirm Download', 'Would you like to download the chart as an image?', function(choice){
+					if(choice == 'yes') {
+						chart.save({
+						type: 'image/png'
+						});
+					}
+				});
+			}
+			}, {
+				text: 'Reload Data',
+				handler: function() {
+				}
+			}],
+			items: chart,
+			//renderTo: loc,
+		});
 	});
 }
 
