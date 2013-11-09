edf43361cfc9307c0548031303a91a5877ef4b7f
Mark Veltzer <mark.veltzer@gmail.com>
2013-11-09 20:34:43 +0200

    another TODO item

--- scripts/audio_cat_mp3_regexp.py
+++ scripts/audio_cat_mp3_regexp.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""
+'''
 Script to be used to catenate many mp3 files.
 
 	Mark Veltzer <mark@veltzer.net>
-"""
+'''
 
 import subprocess
 import glob
@@ -19,32 +19,35 @@ def unite(l, out):
 	else:
 		print(l,out)
 
-'''
 lect=0
 for x in range(1,33):
-	newx="%02d" % (x,)
-	print(newx)
-	l=sorted(glob.glob("%s-*" % (str(newx),)))
+	newx='%02d' % (x,)
+	if doDebug:
+		print(newx)
+	l=sorted(glob.glob('%s-*' % (str(newx),)))
 	if doDebug:
 		print(l)
-	assert len(l)==6
+	assert len(l)==9
 	lect+=1
-	name=l[0][6:]
-	res="%02d - %s" % (lect,name)
-	print("new name is", res)
+	name=l[0][5:]
+	res='%02d - %s' % (lect,name)
+	if doDebug:
+		print('new name is [%s]' % (res))
 	unite(l, res)
+
 '''
 l=[]
 count=1
 lect=0
-for f in sorted(glob.glob("*.mp3")):
+for f in sorted(glob.glob('*.mp3')):
 	l.append(f)
 	if count%6==0:
 		name=f[4:]
 		lect+=1
-		res="%02d - %s" % (lect,name)
+		res='%02d - %s' % (lect,name)
 		unite(l, res)
 		l=[]
 		count=1
 	else:
 		count+=1
+'''
