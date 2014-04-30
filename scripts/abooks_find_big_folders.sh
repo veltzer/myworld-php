71effd92b5edbd2a87f8b01a865957dc2db725c5
Mark Veltzer <mark.veltzer@gmail.com>
2014-04-30 16:56:19 +0300

    more fixes to the scripts

--- scripts/abooks_find_big_folders.sh
+++ scripts/abooks_find_big_folders.sh
@@ -2,4 +2,4 @@
 
 # this script will find folders with large amount of files in them...
 
-find by_name -type d -exec sh -c 'set -- "$0"/*.mp3; [ $# -ge 70 ]' {} \; -print
+find by_name -type d -exec sh -c 'set -- "$0"/*.mp3; [ $# -ge 100 ]' {} \; -print
--- scripts/audio_cat_mp3_regexp.py
+++ scripts/audio_cat_mp3_regexp.py
@@ -4,6 +4,7 @@
 Script to be used to catenate many mp3 files.
 
 	Mark Veltzer <mark@veltzer.net>
+
 '''
 
 import subprocess
@@ -11,14 +12,29 @@ import glob
 
 doRun=True
 doDebug=False
+doCheck=False
+# do you want to redirect standard output?
+doRedirect=False
 
 def unite(l, out):
-	args=[ 'ffmpeg', '-i', 'concat:'+'|'.join(l), '-acodec', 'copy', out ]
+	#args=[ 'ffmpeg', '-i', 'concat:'+'|'.join(l), '-acodec', 'copy', out ]
+	print('creating [%s] out of [%s]' % (out, ','.join(l)))
+	args=[ 'avconv', '-i', 'concat:'+'|'.join(l), '-acodec', 'copy', out, '-loglevel', 'quiet' ]
 	if doRun:
-		subprocess.check_call(args)
+		if doCheck:
+			if doRedirect:
+				subprocess.check_call(args, stdout=open('/dev/null','w'))
+			else:
+				subprocess.check_call(args)
+		else:
+			if doRedirect:
+				subprocess.call(args, stdout=open('/dev/null','w'))
+			else:
+				subprocess.call(args)
 	else:
 		print(l,out)
 
+'''
 lect=1
 for x in range(1,37):
 	newx='%02d' % (x,)
@@ -34,20 +50,37 @@ for x in range(1,37):
 		print('new name is [%s]' % (res))
 	unite(l, res)
 	lect+=1
-
 '''
+
 l=[]
 count=1
 lect=1
 for f in sorted(glob.glob('*.mp3')):
 	l.append(f)
 	if count%6==0:
-		name=f[4:]
+		name=f[5:]
 		res='%02d - %s' % (lect,name)
+		#res='%02d.mp3' % (lect)
 		unite(l, res)
 		l=[]
 		count=1
+		lect+=1
 	else:
 		count+=1
-	lect+=1
+
+'''
+# unify same names
+l=[]
+count=1
+lect=1
+old_name=None
+for f in sorted(glob.glob('*.mp3')):
+	name=f[15:]
+	if name!=old_name and len(l)>0:
+		res='%02d - %s' % (lect,old_name)
+		unite(l, res)
+		lect+=1
+		l=[]
+	old_name=name
+	l.append(f)
 '''
