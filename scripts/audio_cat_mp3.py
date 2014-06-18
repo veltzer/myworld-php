cb797516181b7f2f9991798d367ee41f1214338b
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-18 10:28:48 +0300

    fixed up the script

--- scripts/audio_cat_mp3.py
+++ scripts/audio_cat_mp3.py
@@ -16,5 +16,6 @@ import sys
 if len(sys.argv)<3:
 	raise ValueError('usage: outfile.mp3 [infile1.mp3] [infile2.mp3] ...')
 
-args=['ffmpeg','-i','concat:'+'|'.join(sys.argv[2:]),'-acodec','copy',sys.argv[1]]
+#args=[ 'ffmpeg', '-i', 'concat:'+'|'.join(sys.argv[2:]), '-acodec', 'copy', sys.argv[1]]
+args=[ 'avconv', '-i', 'concat:'+'|'.join(sys.argv[2:]), '-acodec', 'copy', sys.argv[1], '-loglevel', 'quiet' ]
 subprocess.check_call(args)
