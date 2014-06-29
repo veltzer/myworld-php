666d14655dcd8e3b4257987604084bdedc0806fc
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-29 19:09:46 +0300

    some TODO item

--- scripts/utils_find_bad_links.sh
+++ scripts/utils_find_bad_links.sh
@@ -2,4 +2,6 @@
 
 # this is a small script to find bad symbolic links and printout their
 # names.
+# TODO: rewrite this script in python to be more extendble and possible
+# a lot faster.
 find . -type l -and -not -exec test -e {} \; -print
