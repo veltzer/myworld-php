985a46176b20f62141364e65ffd0432e4150dbb4
Mark Veltzer <mark.veltzer@gmail.com>
2014-07-27 12:30:39 +0300

    new script in python

--- scripts/teaching_espeak.sh
+++ scripts/teaching_espeak.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-let x=600
+let x=$1
+let dec=$2
 while [[ $x -gt 0 ]]; do
 	espeak "there are $x second to end of exercise"
 	#espeak "there are $x second to end of course"
 	sleep 60
 	let "x=x-60"
 done
+espeak "the exercise is over!"
