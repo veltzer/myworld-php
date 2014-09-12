b0a652c4bed7711ddd65fe1924e95092d5c05af3
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-12 13:58:37 +0300

    removed redundant script

--- scripts/emovies_find_not_video.sh
+++ scripts/emovies_find_not_video.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # this checks that the files are of the right type
-#find . -type f -and -not \( -name "*.avi" -or -name "*.flv" -or -name "*.mp4" -or -name "*.mov" -or -name "*.ogg" -or -name "*.ogv" -or -name "*.wma" \)
-find . -type f -and -not -name "*.avi"
+find . -mindepth 2 -and -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.mkv"
+#find . -mindepth 2 -type f -and -not \( -name "*.avi" -or -name "*.flv" -or -name "*.mp4" -or -name "*.mov" -or -name "*.ogg" -or -name "*.ogv" -or -name "*.wma" -or -name "*.m4v" -or -name "*.mkv" \)
--- scripts/video_find_not_video.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find . -mindepth 2 -and -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.mkv"
