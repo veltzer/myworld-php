20ce947bbae9a9ff99991c4a5bb01278d408793a
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-12 10:51:12 +0300

    some minor fixes

--- scripts/utils_to_encoding.py
+++ scripts/utils_to_encoding.py
@@ -1,29 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-"""
+'''
 This script converts files given to it to some encoding, utf_8 by default.
 It auto-detects the current encoding, reads using the current
 encoding and writes using the utf-encoding.
-"""
+'''
 
-from __future__ import print_function
 import chardet # for detect
 import sys # for argv
 
 #charset='utf_8';
 charset='UTF-8';
 #charset='ascii';
-write=False
+write=True
+
+# list all encodings that are supported by python
+# CAVEAT: does not list encodings that do not have aliases
+#import encodings.aliases # for aliases
+#print(encodings.aliases.aliases.keys())
+#exit(1)
 
 for filename in sys.argv[1:]:
 	print('doing file',filename)
-	with open(filename,"r") as f:
+	with open(filename,'r') as f:
 		b=f.read()
 		h=chardet.detect(b);
 		origcharset=h['encoding']
 		print('detect is',h['encoding'])
 		new_content=b.decode(origcharset).encode(charset)
 	if write:
-		with open(filename,"w") as f:
+		with open(filename,'w') as f:
 			f.write(new_content)
-		
