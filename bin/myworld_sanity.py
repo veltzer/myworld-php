097290c062258df881a3edf1b3eca151a5233aa3
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-24 07:15:49 +0300

    started sanity script in python

---
+++ bin/myworld_sanity.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+'''
+This script checks the sanity of the myworld database.
+
+# here is what it should check:
+# 1. AUTO_INCREMENT always implies tight packing of the table.
+# 2. no works without views.
+# 3. no works without reviews.
+# 4. no works without lengths.
+# 5. no person without single group representing him.
+# 6. no works without people involved (at least one).
+# 7. no person without external id of some sort (at least one).
+# more to come...
+
+Currently it does very little.
+'''
+
+#############
+# libraries #
+#############
+import myworld.db # for connect, get_results
+
+##############
+# parameters #
+##############
+
+########
+# code #
+########
+conn=myworld.db.connect()
+
+sql='''
+SELECT TbWkWork.id,TbWkWork.name FROM TbWkWork, TbWkWorkType WHERE
+	length IS NULL AND
+	updatedLengthDate IS NULL
+	AND TbWkWork.typeId=TbWkWorkType.id
+	AND TbWkWorkType.isVideo=TRUE
+'''
+res=myworld.db.get_results(conn, sql)
+if len(res)>0:
+	print('got works with no length')
+	for i in range(10):
+		print(res[i]['name'])
+
+conn.close()
