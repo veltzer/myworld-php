995d63a86eb52e25c9f515e8472907e766e73699
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-25 17:21:04 +0300

    sanity is getting better

--- bin/myworld_sanity.py
+++ bin/myworld_sanity.py
@@ -30,6 +30,7 @@ import myworld.db # for connect, get_results
 ########
 conn=myworld.db.connect()
 
+print('checking videos without length updated')
 sql='''
 SELECT TbWkWork.id,TbWkWork.name FROM TbWkWork, TbWkWorkType WHERE
 	length IS NULL AND
@@ -38,10 +39,8 @@ SELECT TbWkWork.id,TbWkWork.name FROM TbWkWork, TbWkWorkType WHERE
 	AND TbWkWorkType.isVideo=TRUE
 '''
 results=myworld.db.get_results(conn, sql)
-if len(results)>0:
-	print('got works with no length')
-	for result in results:
-		print('\t{0}'.format(result['name']))
+for result in results:
+	print('\t{0}'.format(result['name']))
 
 print('checking bad work names')
 sql='''
@@ -52,6 +51,6 @@ for result in results:
 	f_id=result['id']
 	f_name=result['name']
 	if f_name.strip()!=f_name:
-		print('got bad name of work for id [{0}] and name [{1}]'.format(f_id, f_name))
+		print('\tgot bad name of work for id [{0}] and name [{1}]'.format(f_id, f_name))
 
 conn.close()
