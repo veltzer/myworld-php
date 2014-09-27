1d9a286d5dc4fcf3d119cda31dd99c5e500e2ccb
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-27 07:24:13 +0300

    nicer SQL queries

--- bin/myworld_sanity.py
+++ bin/myworld_sanity.py
@@ -30,10 +30,13 @@ import myworld.db # for connect, get_results
 ########
 conn=myworld.db.connect()
 
-print('checking videos without length updated')
+print('checking works which are videos without length updated')
 sql='''
-SELECT TbWkWork.id,TbWkWork.name FROM TbWkWork, TbWkWorkType WHERE
-	length IS NULL AND
+SELECT
+	TbWkWork.id,TbWkWork.name
+FROM
+	TbWkWork, TbWkWorkType
+WHERE
 	updatedLengthDate IS NULL
 	AND TbWkWork.typeId=TbWkWorkType.id
 	AND TbWkWorkType.isVideo=TRUE
@@ -42,9 +45,15 @@ results=myworld.db.get_results(conn, sql)
 for result in results:
 	print('\t{0}'.format(result))
 
-print('checking bad work names')
+print('checking works with space in the begining or end of name')
 sql='''
-SELECT TbWkWork.id,TbWkWork.name FROM TbWkWork WHERE name REGEXP '^[[:space:]].*$' OR name REGEXP '^.*[[:space:]]$'
+SELECT
+	TbWkWork.id,TbWkWork.name
+FROM
+	TbWkWork
+WHERE
+	name REGEXP '^[[:space:]].*$' OR
+	name REGEXP '^.*[[:space:]]$'
 '''
 results=myworld.db.get_results(conn, sql)
 for result in results:
@@ -52,31 +61,47 @@ for result in results:
 
 print('checking for people that do not have external ids and are not friends')
 sql='''
-SELECT TbIdPerson.id, TbIdPerson.firstname, TbIdPerson.surname FROM TbIdPerson
-WHERE NOT EXISTS (
-	SELECT TbIdPersonExternal.id
-	FROM TbIdPersonExternal
-	WHERE TbIdPersonExternal.personId=TbIdPerson.id
-) AND NOT EXISTS (
-	SELECT TbIdGrpPerson.id
-	FROM TbIdGrpPerson, TbIdGrp
-	WHERE TbIdGrpPerson.personId=TbIdPerson.id AND
-	TbIdGrpPerson.groupId=TbIdGrp.id AND
-	TbIdGrp.name='friends'
-)
+SELECT
+	TbIdPerson.id, TbIdPerson.firstname, TbIdPerson.surname
+FROM
+	TbIdPerson
+WHERE
+	NOT EXISTS (
+		SELECT TbIdPersonExternal.id
+		FROM TbIdPersonExternal
+		WHERE TbIdPersonExternal.personId=TbIdPerson.id
+	) AND NOT EXISTS (
+		SELECT TbIdGrpPerson.id
+		FROM TbIdGrpPerson, TbIdGrp
+		WHERE TbIdGrpPerson.personId=TbIdPerson.id AND
+		TbIdGrpPerson.groupId=TbIdGrp.id AND
+		TbIdGrp.name='friends'
+	)
 '''
 results=myworld.db.get_results(conn, sql)
 for result in results:
+	f_id=result['id']
 	print('\t{0}'.format(result))
+	sql2='''SELECT TbWkWork.name
+		FROM TbWkWork, TbWkWorkContrib
+		WHERE TbWkWork.id=TbWkWorkContrib.workId AND
+		TbWkWorkContrib.personId='''+str(f_id)
+	results2=myworld.db.get_results(conn, sql2)
+	for result2 in results2:
+		print('\t\t{0}'.format(result2))
 
 print('checking for works that do not have external ids')
 sql='''
-SELECT TbWkWork.id, TbWkWork.name FROM TbWkWork
-WHERE NOT EXISTS (
-	SELECT TbWkWorkExternal.id
-	FROM TbWkWorkExternal
-	WHERE TbWkWorkExternal.workId=TbWkWork.id
-)
+SELECT
+	TbWkWork.id, TbWkWork.name
+FROM
+	TbWkWork
+WHERE
+	NOT EXISTS (
+		SELECT TbWkWorkExternal.id
+		FROM TbWkWorkExternal
+		WHERE TbWkWorkExternal.workId=TbWkWork.id
+	)
 '''
 results=myworld.db.get_results(conn, sql)
 for result in results:
