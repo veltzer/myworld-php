2e022fc3ae35e5266f88862bb64c123567444f45
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-19 22:41:05 +0300

    started better writing of the perl scripts

--- bin/mysql_create_slug.pl
+++ bin/mysql_create_slug.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl -w
 
-# this script creates a slug in the database.
-# give it a table, a field and a field to put the slug in and it will do the rest.
+=head
+
+this script creates a slug in the database.
+give it a table, a field and a field to put the slug in and it will do the rest.
+
+=cut
 
 use strict;
 use diagnostics;
@@ -12,39 +16,51 @@ use File::Basename qw();
 use Perl6::Slurp qw();
 
 # parameters
-my($param_table)="TbBsCompanies";
-my($param_field_from)="name";
-my($param_field_to)="slug";
+my($param_table)='TbBsCompanies';
+my($param_field_from)='name';
+my($param_field_to)='slug';
+my($param_host)=$ARGV[0];
+my($param_port)=$ARGV[1];
+my($param_user)=$ARGV[2];
+my($param_pass)=$ARGV[3];
+my($param_name)=$ARGV[4];
 
 my($dbh);
 
 sub to_slug($) {
 	my($val)=$_[0];
-	return join("-",map(lc,split(" ",$val)));
+	return join('-',map(lc,split(' ',$val)));
 }
 
 sub handle_error() {
 	my($rc)=$dbh->err;
 	my($str)=$dbh->errstr;
 	my($rv)=$dbh->state;
-	throw Error::Simple($str.",".$rv.",".$rv);
+	throw Error::Simple($str.','.$rv.','.$rv);
 }
 
-$dbh=DBI->connect('dbi:mysql:myworld','','',{
+$dsn='dbi:mysql:'.$param_name;
+if (defined($param_host)) {
+	$dns.=';host='.$param_host;
+}
+if (defined($param_port)) {
+	$dns.=';=port'.$param_port;
+}
+$dbh=DBI->connect($dsn, $param_user, $param_pass, {
 	RaiseError => 1,
 	AutoCommit => 0,
 	mysql_enable_utf8 => 1,
 });
 $dbh->{HandleError} =\&handle_error;
 
-my($sql)="select $param_field_from from $param_table";
+my($sql)='SELECT $param_field_from FROM $param_table';
 my($sth)=$dbh->prepare($sql);
 $sth->execute();
 my($rowhashref);
 while($rowhashref=$sth->fetchrow_hashref()) {
 	my($field_val)=$rowhashref->{$param_field_from};
 	my($new_slug)=to_slug($field_val);
-	$dbh->do("update $param_table set $param_field_to=? where $param_field_from=?",
+	$dbh->do('UPDATE $param_table SET $param_field_to=? WHERE $param_field_from=?',
 		undef,$new_slug,$field_val);
 }
 
--- bin/mysql_get_fields.pl
+++ bin/mysql_get_fields.pl
@@ -1,42 +1,84 @@
 #!/usr/bin/perl -w
 
-# A script that tests a function to read all fields from a table name
+=head
+
+A script that prints the names of the columns in a table
+
+=cut
+
+# uses
 
 use strict;
 use diagnostics;
 use DBI;
+use Data::Dumper;
+use Getopt::Long;
+use Config::IniFiles;
+use File::HomeDir;
 
-# parameters...
+# parameters
 
-my($db_name)='myworld';
-#my($db_name)='myworld';
-my($db_user)='';
-my($db_pass)='';
-my($db_table_name)='TbWkWork';
 my($debug)=1;
 
-# here we go...
+# code
 
-sub get_fields($$$) {
-	my($dbh,$schema,$table)=@_;
-	if($debug) {
-		print "schema is $schema\n";
-		print "table is $table\n";
+sub db_connect($) {
+	my($rcfile)=@_;
+	my($cfg);
+	$cfg=Config::IniFiles->new( -file => $rcfile ) || die('unable to access ini file '.$rcfile);
+	my($param_user)=$cfg->val('db', 'user');
+	my($param_pass)=$cfg->val('db', 'pass');
+	my($param_host)=$cfg->val('db', 'host');
+	my($param_port)=$cfg->val('db', 'port');
+	my($param_name)=$cfg->val('db', 'name');
+
+	my($dsn)='dbi:mysql:'.$param_name;
+	if(defined($param_host)) {
+		$dsn.=';host='.$param_host;
 	}
-	my($sql)='select column_name from information_schema.columns where table_name="'.$table.'" and table_schema="'.$schema.'"';
+	if(defined($param_port)) {
+		$dsn.=';=port'.$param_port;
+	}
+	my($dbh)=DBI->connect($dsn, $param_user, $param_pass, {
+		RaiseError => 1,
+		AutoCommit => 0,
+		mysql_enable_utf8 => 1,
+	}) or die 'Connection Error: '.$DBI::errstr;
+	#$dbh->{HandleError} =\&handle_error;
+	return $dbh;
+}
+
+sub get_fields($$) {
+	my($dbh, $table)=@_;
+	if($debug) {
+		print 'table is '.$table."\n";
+	}
+	my($sql)='SELECT column_name FROM information_schema.columns WHERE table_name=? AND table_schema=DATABASE()';
 	my($sth)=$dbh->prepare($sql);
-	$sth->execute() or die "SQL Error: $DBI::errstr\n";
+	$sth->execute($table) or die 'SQL Error: '.$DBI::errstr;
 	my($rowhashref);
 	my(%hash);
 	while($rowhashref=$sth->fetchrow_hashref()) {
 		my($column_name)=$rowhashref->{'column_name'};
 		$hash{$column_name}=defined;
 	}
-	return \$hash;
+	if($debug) {
+		print(Dumper(\%hash));
+	}
+	return \%hash;
 }
 
-my($dbh)=DBI->connect('dbi:mysql:'.$db_name,$db_user,$db_pass,{ RaiseError => 1 }) or die "Connection Error: $DBI::errstr\n";
+my($param_rcfile)=File::HomeDir->my_home.'/.myworldrc';
+my($param_table)=undef;
+GetOptions(
+	'rcfile=s' => \$param_rcfile,
+	'table=s' => \$param_table,
+) or die 'error in command line parsing';
 
-my($hash)=get_fields($dbh,$db_name,$db_table_name);
+if (!defined($param_table)) {
+	die 'must set table using --table';
+}
 
+my($dbh)=db_connect($param_rcfile);
+my($hash)=get_fields($dbh, $param_table);
 $dbh->disconnect();
