dfcfa8fb98a298a60694d6d7e6d0eba4adf2b4f1
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-20 16:25:05 +0300

    removed stuff that should not be here

--- apache_ssl/install_keys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo scp veltzer.pem /etc/ssl/certs
-sudo scp veltzer.key /etc/ssl/private
--- tests/test_php.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* this is a test script to check that php support is working fine... */
-echo "If you can see this then PHP is working on your apache server...";
-?>
