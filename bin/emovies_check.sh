f82cdb4e13b11ba92d47d4ccd34a775ac14edb43
Mark Veltzer <mark.veltzer@gmail.com>
2014-12-06 03:24:43 +0200

    wmv in emovies

--- bin/emovies_check.sh
+++ bin/emovies_check.sh
@@ -2,7 +2,7 @@
 
 # this checks that the files are of the right type
 echo "Checking that all are of the right suffix"
-find . -mindepth 2 -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.mkv"
+find . -mindepth 2 -type f -and -not -name "*.avi" -and -not -name "*.m4v" -and -not -name "*.mp4" -and -not -name "*.mkv" -and -not -name "*.wmv"
 # this checks for permissions other than 444
 echo "Permission problems"
 find . -mindepth 2 -type f -and -not -perm 444
