260aa936e84fbf6cdbfbede5966bc810e38e6baa
Mark Veltzer <mark.veltzer@gmail.com>
2014-12-26 15:40:27 +0200

    added a generic open url script

---
+++ bin/utils_open.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+This is a wrapper for gnome-open(1) which does not pollute the screen.
+'''
+
+import sys # for argv, exit
+import subprocess # for call, DEVNULL
+
+args=[
+	'gnome-open',
+]
+# give all command line args passed to the wrapper excluding the first (the program name)
+args.extend(sys.argv[1:])
+sys.exit(subprocess.call(args), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
