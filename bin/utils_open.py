fef7798cc63f951f6789f85c5a6069f74b38a17b
Mark Veltzer <mark.veltzer@gmail.com>
2014-12-26 16:07:02 +0200

    a better open utility

--- bin/utils_open.py
+++ bin/utils_open.py
@@ -4,12 +4,10 @@
 This is a wrapper for gnome-open(1) which does not pollute the screen.
 '''
 
-import sys # for argv, exit
+import sys # for argv
 import subprocess # for call, DEVNULL
 
-args=[
-	'gnome-open',
-]
-# give all command line args passed to the wrapper excluding the first (the program name)
+args=['/usr/bin/gnome-open']
 args.extend(sys.argv[1:])
-sys.exit(subprocess.call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+ret=subprocess.call(args, stderr=subprocess.DEVNULL)
+assert ret==0
