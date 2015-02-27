40d26881cc299ea3e485ff8b1984622caac3df5e
Mark Veltzer <mark.veltzer@gmail.com>
2015-02-27 20:24:03 +0200

    fix up the script not to produce errors

--- bin/myworld_backup.sh
+++ bin/myworld_backup.sh
@@ -12,25 +12,26 @@
 # PARAMETERS
 FOLDER=`date +%y%m%d%H%M%S`
 DIR=~/Dropbox/db/$FOLDER
-DB=myworld
+DBS="--databases myworld"
+DB="--database=myworld"
 MYSQL_DATE=`date +'%F %T'`
 
 # BODY
 # insert the data of this backup into the database
-mysql --execute="INSERT INTO TbBackup (backupDate) values('$MYSQL_DATE')"
+mysql $DB --execute="INSERT INTO TbBackup (backupDate) values('$MYSQL_DATE')"
 # lets make sure we have the output dir
 mkdir $DIR
 #OPTS="--complete-insert --skip-dump-date"
 OPTS="--complete-insert"
 # data and schema
-mysqldump $OPTS $DB | gzip > $DIR/data_and_schema.sql.gz
+mysqldump $OPTS $DBS | gzip > $DIR/data_and_schema.sql.gz
 # only data
 #mysqldump $OPTS $DB --no-create-info | gzip > $DIR/data_only.sql.gz
 # only schema
-mysqldump $OPTS $DB --no-data | gzip > $DIR/schema_only.sql.gz
+mysqldump $OPTS $DBS --no-data | gzip > $DIR/schema_only.sql.gz
 # only stats
 mysql_stats.sh | gzip > $DIR/stats.txt.gz
 
 # backup the blog
-mysqldump $OPTS wordpress | gzip > $DIR/wordpress_data_and_schema.sql.gz
+mysqldump $OPTS --databases wordpress | gzip > $DIR/wordpress_data_and_schema.sql.gz
 tar jcfP $DIR/blog.tar.bz2 /var/www/blog
