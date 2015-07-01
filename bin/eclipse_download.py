838572ad9eb6ee64809642f56e70fa28d59006b9
Mark Veltzer <mark.veltzer@gmail.com>
2015-07-01 14:14:04 +0300

    adjusted for new eclipse

--- bin/eclipse_download.py
+++ bin/eclipse_download.py
@@ -16,6 +16,8 @@ protocol='http'
 mirror='mirror.hust.edu.cn'
 mirror='mirrors.hustunique.com'
 mirror='kambing.ui.ac.id'
+mirror='ftp.jaist.ac.jp'
+mirror='http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release'
 products=[
 	'automotive',
 	'cpp',
@@ -46,8 +48,10 @@ incubations=[
 	False,
 	False,
 ]
-version='SR2'
-release='luna'
+#version='SR2'
+version='R'
+#release='luna'
+release='mars'
 platforms=[
 	'-x86_64',
 	'',
@@ -59,6 +63,7 @@ for product, incubation in zip(products, incubations):
 	else:
 		incubation_str=''
 	for platform in platforms:
-		url='{protocol}://{mirror}/eclipse/technology/epp/downloads/release/{release}/{version}/eclipse-{product}-{release}-{version}{incubation_str}-linux-gtk{platform}.tar.gz'.format(**vars())
+		#url='{protocol}://{mirror}/eclipse/technology/epp/downloads/release/{release}/{version}/eclipse-{product}-{release}-{version}{incubation_str}-linux-gtk{platform}.tar.gz'.format(**vars())
+		url='{mirror}/{release}/{version}/eclipse-{product}-{release}-{version}{incubation_str}-linux-gtk{platform}.tar.gz'.format(**vars())
 		filename='eclipse-{product}-{release}-{version}-linux-gtk{platform}.tar.gz'.format(**vars())
 		download.generic.get(url, filename)
