73069389592f0d766151abe25feaca93aa5b9f10
Mark Veltzer <mark.veltzer@gmail.com>
2015-10-12 08:06:56 +0300

    this script now checks checksums

--- bin/eclipse_download.py
+++ bin/eclipse_download.py
@@ -6,13 +6,21 @@ This is a script that knows how to download eclipse.
 These are examples of urls to download
 http://kambing.ui.ac.id/eclipse/technology/epp/downloads/release/luna/SR2/eclipse-cpp-luna-SR2-linux-gtk-x86_64.tar.gz
 http://mirrors.hustunique.com/eclipse/technology/epp/downloads/release/luna/SR2/eclipse-cpp-luna-SR2-linux-gtk.tar.gz
-
-TODO:
-- make this script check the sha1 checksums as well as downloading the packages.
 '''
 
 import download.generic # for get
 import os.path # for isfile
+import hashlib # for new
+
+def hexdigest(filename, algo):
+	BLOCKSIZE = 65536
+	hasher = hashlib.new(algo)
+	with open(filename, 'rb') as afile:
+		buf = afile.read(BLOCKSIZE)
+		while len(buf) > 0:
+			hasher.update(buf)
+			buf = afile.read(BLOCKSIZE)
+	return hasher.hexdigest()
 
 protocol='http'
 mirror='http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/downloads/release'
@@ -38,6 +46,7 @@ platforms=[
 	'-x86_64', # x64
 	'', # i386
 ]
+cs_type='sha512'
 
 for product, incubation in products:
 	if incubation:
@@ -46,8 +55,23 @@ for product, incubation in products:
 		incubation_str=''
 	for platform in platforms:
 		url='{mirror}/{release}/{version}/eclipse-{product}-{release}-{version}{incubation_str}-linux-gtk{platform}.tar.gz'.format(**vars())
+		cs_url='{mirror}/{release}/{version}/eclipse-{product}-{release}-{version}{incubation_str}-linux-gtk{platform}.tar.gz.{cs_type}'.format(**vars())
 		filename='eclipse-{product}-{release}-{version}-linux-gtk{platform}.tar.gz'.format(**vars())
+		cs_filename='eclipse-{product}-{release}-{version}-linux-gtk{platform}.tar.gz.{cs_type}'.format(**vars())
 		if os.path.isfile(filename):
-			print('skipping [{0}]...'.format(filename))
+			print('skipping [{0}] from [{1}]...'.format(filename, url))
 		else:
 			download.generic.get(url, filename)
+		if os.path.isfile(cs_filename):
+			print('skipping [{0}] from [{1}]...'.format(cs_filename, cs_url))
+		else:
+			download.generic.get(cs_url, cs_filename)
+		# read the checksum and compare
+		with open(cs_filename) as f:
+			read_cs=f.readline().split()[0]
+			calc_cs=hexdigest(filename, cs_type)
+			if read_cs!=calc_cs:
+				print('checksum for [{0}] is bad...'.format(filename))
+				sys.exit(1)
+			else:
+				print('checksum for [{0}] is good...'.format(filename))
