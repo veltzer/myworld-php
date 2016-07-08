de876547a2b59b487c7d2be32649f40b02650ef6
Mark Veltzer <mark.veltzer@gmail.com>
2016-07-08 17:43:42 +0300

    fixed up this script

---
+++ bin/eclipse_open_me.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+This script runs a browser on the output of the current project
+'''
+
+###########
+# imports #
+###########
+import subprocess # for check_call
+
+##############
+# parameters #
+##############
+# project
+project=os.getcwd().split('/')[-1]
+
+########
+# code #
+########
+subprocess.check_call([
+	'gnome-open',
+	'https://localhost:8443/{project}'.format(project=project),
+])
