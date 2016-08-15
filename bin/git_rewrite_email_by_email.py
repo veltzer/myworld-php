6dcc0d858ae12afb7a511ec9843bd1b08d452da6
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-15 15:42:40 +0300

    next version of this script

--- bin/git_rewrite_email_by_email.py
+++ bin/git_rewrite_email_by_email.py
@@ -11,15 +11,30 @@ if len(sys.argv)!=1:
     print('{0}: usage: {0}'.format(sys.argv[0]), file=sys.stderr)
     sys.exit(1)
 
-old_email='mark@veltzer.net'
-new_email='mark.veltzer@gmail.com'
-expr='''if [ "$GIT_COMMITTER_EMAIL" = "{old_email}" ];
-then
-	GIT_AUTHOR_EMAIL="{new_email}";
-	git commit-tree "$@";
-else
-	git commit-tree "$@";
-fi'''.format(**locals())
-# --force is there to override old backup of 'git filter-branch'
-args=['git','filter-branch','--force','--commit-filter',expr]
-subprocess.check_call(args)
+old_emails=[
+    'mark@veltzer.net',
+    'mark@twiggle.com',
+    'mark.veltzer@gmail.net',
+]
+for old_email in old_emails:
+    new_email='mark.veltzer@gmail.com'
+    expr='''if [ "$GIT_COMMITTER_EMAIL" = "{old_email}" ];
+    then
+            GIT_AUTHOR_EMAIL="{new_email}";
+            git commit-tree "$@";
+    else
+            git commit-tree "$@";
+    fi'''.format(**locals())
+    # --force is there to override old backup of 'git filter-branch'
+    args=['git','filter-branch','--force','--commit-filter',expr]
+    subprocess.check_call(args)
+
+# commit everything
+subprocess.check_call([
+    'git',
+    'push',
+    '--force',
+    '--tags',
+    'origin',
+    'refs/heads/*',
+])
