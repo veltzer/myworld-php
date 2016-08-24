080190890d8eaea7e89fb1b94638deaf31ee31ff
Mark Veltzer <mark.veltzer@gmail.com>
2016-08-24 13:10:27 +0300

    small fix

--- bin/devel_install_css_validator.sh
+++ bin/devel_install_css_validator.sh
@@ -7,4 +7,5 @@ This script installs the tip of the css-validator project
 COMMENT
 
 git clone --depth 1 --branch master git@github.com:w3c/css-validator.git
-cd css-validator; ant
+cd css-validator
+ant
