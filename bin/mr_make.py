2afca7b30c8cc2facff346a3d68b88ce87485842
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-17 13:25:39 +0300

    unify code

--- bin/mr_make.py
+++ bin/mr_make.py
@@ -8,29 +8,30 @@ that is mine.
 import os.path # for expanduser, isdir, join
 import utils.github # for get_nonforked_repos
 
+home=os.getenv('HOME')
+
 '''
-have_mrconfig=set()
+projects=list()
 filename=os.path.expanduser('~/.mrconfig')
 for line in open(filename):
     line=line.rstrip()
     if line.startswith('['):
-        project=line[1:-1].split('/')[-1]
-        have_mrconfig.add(project)
+        project_root=os.path.join(home, line[1:-1])
+        project_name=line[1:-1].split('/')[-1]
+        projects.append((project_name, project_root))
 '''
 
-home=os.getenv('HOME')
+projects=[(repo.name, os.path.join(home,'git',repo.name)) for repo in utils.github.get_nonforked_repos_list()]
 
-for repo in utils.github.get_nonforked_repos_list():
-    project_root=os.path.join(home,'git',repo.name)
+for project_name, project_root in projects:
+    print('building [{0}] at [{1}]...'.format(project_name, project_root))
     makefile=os.path.join(project_root, 'Makefile')
     bootstrap=os.path.join(project_root, 'bootstrap')
     if os.path.isfile(makefile):
-        print('building [{0}]...'.format(repo.name))
         os.chdir(project_root)
         os.system('make')
     elif os.path.isfile(bootstrap):
-        print('building [{0}]...'.format(repo.name))
         os.chdir(project_root)
         os.system('./bootstrap')
     else:
-        print('dont know how to build [{0}]...'.format(repo.name))
+        print('dont know how to build [{0}]...'.format(project_name))
