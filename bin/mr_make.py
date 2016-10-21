c70f720005b9057abff191eca1480eedd8485439
Mark Veltzer <mark.veltzer@gmail.com>
2016-10-22 01:46:35 +0300

    better script

--- bin/mr_make.py
+++ bin/mr_make.py
@@ -5,12 +5,13 @@ This script runs 'make' (make(1)) in every project
 that is mine.
 '''
 
-import os.path # for expanduser, isdir, join
+import os # for chdir, system
+import os.path # for expanduser, isdir, isfile, join
 import utils.github # for get_nonforked_repos
+import subprocess # for check_call
 
 home=os.getenv('HOME')
 
-'''
 projects=list()
 filename=os.path.expanduser('~/.mrconfig')
 for line in open(filename):
@@ -19,19 +20,22 @@ for line in open(filename):
         project_root=os.path.join(home, line[1:-1])
         project_name=line[1:-1].split('/')[-1]
         projects.append((project_name, project_root))
-'''
 
-projects=[(repo.name, os.path.join(home,'git',repo.name)) for repo in utils.github.get_nonforked_repos_list()]
+#projects=[(repo.name, os.path.join(home,'git',repo.name)) for repo in utils.github.get_nonforked_repos_list()]
 
 for project_name, project_root in projects:
+    if not os.path.isdir(project_root):
+        continue
     print('building [{0}] at [{1}]...'.format(project_name, project_root))
     makefile=os.path.join(project_root, 'Makefile')
     bootstrap=os.path.join(project_root, 'bootstrap')
     if os.path.isfile(makefile):
         os.chdir(project_root)
-        os.system('make')
+        subprocess.check_call(['make'])
+        #os.system('make')
     elif os.path.isfile(bootstrap):
         os.chdir(project_root)
-        os.system('./bootstrap')
+        subprocess.check_call(['./bootstrap'])
+        #os.system('./bootstrap')
     else:
         print('dont know how to build [{0}]...'.format(project_name))
