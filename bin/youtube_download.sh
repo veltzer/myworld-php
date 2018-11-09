1ef545994415f4f7d0f97013edb587c99cb937ef
Mark Veltzer <mark.veltzer@gmail.com>
2018-11-09 02:40:44 +0200

    added scripts

---
+++ bin/youtube_download.sh
@@ -0,0 +1,4 @@
+youtube-dl\
+	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
+	--continue\
+	--ignore-errors
---
+++ bin/youtube_download_some.sh
@@ -0,0 +1,6 @@
+youtube-dl\
+	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
+	--continue\
+	--ignore-errors\
+	--playlist-end 30
+#	--playlist-start 15
