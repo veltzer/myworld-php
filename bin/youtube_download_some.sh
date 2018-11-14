1710c284e4b6e0575af9d9bbf49f0e2ab484334a
Mark Veltzer <mark.veltzer@gmail.com>
2018-11-14 04:55:10 +0200

    added hibernation support

--- bin/youtube_download_some.sh
+++ bin/youtube_download_some.sh
@@ -1,6 +1,8 @@
+utils_hibernate_disable.sh
 youtube-dl\
 	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
 	--continue\
 	--ignore-errors\
 	--playlist-end 30
 #	--playlist-start 15
+utils_hibernate_enable.sh
