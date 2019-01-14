513ead0c2220c0b9a594dd68da95d06624d45caf
Mark Veltzer <mark.veltzer@gmail.com>
2019-01-14 18:14:33 +0200

    60 is safer

--- bin/youtube_download_some.sh
+++ bin/youtube_download_some.sh
@@ -3,6 +3,6 @@ youtube-dl\
 	https://www.youtube.com/playlist?list=PLZHKR1yYunFQ3ciN2kHYg5y7YjKZUjMey\
 	--continue\
 	--ignore-errors\
-	--playlist-end 30
+	--playlist-end 60
 #	--playlist-start 15
 utils_hibernate_enable.sh
