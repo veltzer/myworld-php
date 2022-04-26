c1867613719978c3c9a95d9a29bf40fc0b966b70
Mark Veltzer <mark.veltzer@gmail.com>
2022-04-27 00:33:00 +0300

    better project config

--- config/project.py
+++ config/project.py
@@ -1,14 +1,4 @@
-"""
-project definitions for templar
-"""
-
-def populate(d):
-    d.project_name="myworld"
-    d.project_long_description="life feed project"
-    d.project_year_started="2011"
-    d.project_description="This is a collection of scripts and wordpress plugins that I use for my own databases."
-
-def get_deps():
-    return [
-        __file__, # myself
-    ]
+project_name="myworld"
+project_long_description="life feed project"
+project_year_started=2011
+project_description="This is a collection of scripts and wordpress plugins that I use for my own databases."
