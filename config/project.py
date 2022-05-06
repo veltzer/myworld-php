53f8e5df04080fcbb242209395dae932d66411d6
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 03:22:57 +0300

    fixing the build

--- config/project.py
+++ config/project.py
@@ -1,6 +1,6 @@
-project_name="myworld"
+project_name="myworld-php"
 project_long_description="life feed project"
-project_year_started=2011
+project_year_started=2010
 project_description="This is a collection of scripts and wordpress plugins that I use for my own databases."
 
 import config.helpers
