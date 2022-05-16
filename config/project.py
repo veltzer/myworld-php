add7d4609ef293ed014eb9c25fc18bf7db813c8c
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-16 23:22:44 +0300

    new version

--- config/project.py
+++ config/project.py
@@ -1,7 +1,8 @@
+import pydmt.helpers.signature
+
 project_name="myworld-php"
 project_long_description="life feed project"
 project_year_started=2010
 project_description="This is a collection of scripts and wordpress plugins that I use for my own databases."
 
-import config.helpers
-project_copyright_years = config.helpers.get_copyright_years(project_year_started)
+project_copyright_years = pydmt.helpers.signature.get_copyright_years(project_year_started)
