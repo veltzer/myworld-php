f35175d07009485e03f69462f6749deaaeb3a163
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-19 13:46:44 +0200

    keywords update

--- config/project.py
+++ config/project.py
@@ -1,4 +1,9 @@
 name = "myworld-php"
 description_short="life feed project"
 description_long="This is a collection of scripts and wordpress plugins that I use for my own databases."
-keywords = []
+keywords = [
+    "myworld",
+    "php",
+    "html",
+    "javascript",
+]
