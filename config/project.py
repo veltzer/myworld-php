7afd86330c1b86cb6152ff7f079cd0523ff15b29
Mark Veltzer <mark.veltzer@gmail.com>
2025-03-22 21:27:52 +0200

    linting

--- config/project.py
+++ config/project.py
@@ -1,3 +1,4 @@
+""" project definitions """
 name = "myworld-php"
 description_short="life feed project"
 description_long="This is a collection of scripts and wordpress plugins that I use for my own databases."
